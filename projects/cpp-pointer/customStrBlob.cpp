8f869d28154d095260fd111737c5e614b202a43c
dingguoliang <dinggl9367@163.com>
2020-11-21 11:20:02 +0800

    add common pointer markdown

---
+++ projects/cpp-pointer/customStrBlob.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dingguoliang on 2020/11/11.
+//
+
+#include "customStrBlob.h"
---
+++ projects/cpp-pointer/customStrBlob.h
@@ -0,0 +1,14 @@
+//
+// Created by dingguoliang on 2020/11/11.
+//
+
+#ifndef CPP_POINTER_CUSTOMSTRBLOB_H
+#define CPP_POINTER_CUSTOMSTRBLOB_H
+
+
+class customStrBlob {
+
+};
+
+
+#endif //CPP_POINTER_CUSTOMSTRBLOB_H
